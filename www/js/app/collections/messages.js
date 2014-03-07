f720842719c6641d26346dd47769886d478e88d3
Ryan Sepassi <rsepassi@gmail.com>
2014-03-07 15:01:05 -0800

    Added messages collection and model

---
+++ www/js/app/collections/messages.js
@@ -0,0 +1,12 @@
+define(function (require) {
+  var Backbone = require('Backbone'),
+      MessageModel = require('app/models/message');
+
+  return Backbone.Collection.extend({
+    url: function () {
+      return '/friends/' + this._friend_id + '/messages'
+    },
+
+    model: MessageModel
+  });
+});
