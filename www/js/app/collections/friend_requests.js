6c61068c450ea07966b3ee2c7c7717631dc8b2ef
Ryan Sepassi <rsepassi@gmail.com>
2014-03-07 16:12:42 -0800

    Added friend request collection and model

---
+++ www/js/app/collections/friend_requests.js
@@ -0,0 +1,11 @@
+define(function (require) {
+  "use strict";
+
+  var Backbone = require('backbone'),
+      FriendRequestModel = require('app/models/friend_request');
+
+  return Backbone.Collection.extend({
+    url: '/friend_requests',
+    model: FriendRequestModel
+  });
+});
